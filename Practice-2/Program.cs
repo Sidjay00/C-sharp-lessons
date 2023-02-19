0afa912f7a5e4d24870a564e2fda3a0cb07c03eb
K.Sidorov <2887458@gmail.com>
2023-02-19 12:29:04 +0300

    some solution

--- Practice-2/Program.cs
+++ Practice-2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int z = ConsoleTakeInt();
+
+Print(z.ToString());
+
+int ConsoleTakeInt()
+{
+    int number;
+    Console.Write($"Введите число: ");
+    bool flag = int.TryParse(Console.ReadLine(), out number);
+    if (flag)
+        return number;
+    else
+    {
+        Console.Write($"Введено не число\n");
+        return ConsoleTakeInt();
+    }
+}
+
+void Print(string text)
+{
+    int length = text.Length;
+    string border = "--";
+    string text2 = "|" + text + "|";
+    for (int i = 0; i < length; i++)
+        border += "-";
+    Console.Write($"{border}\n{text2}\n{border}\n");
+}
