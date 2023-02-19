e4f2e196387b26f7872d0b925ee1d9911f05b5aa
K.Sidorov <2887458@gmail.com>
2023-02-19 22:16:07 +0300

    added a solution for the task

--- Practice-2-delete-the-second-number/Program.cs
+++ Practice-2-delete-the-second-number/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру числа.
+
+int number;
+
+number = new Random().Next(100, 1000);
+
+int a = number % 10;
+int b = number / 100;
+
+// int number2 = b * 10 + a;
+
+// Console.WriteLine($"{number} -> {number2}");
+
+// решение через строку
+string text = b.ToString() + a.ToString();
+
+Console.WriteLine($"{number} -> {text}"); //этот пример я решил самостоятельно
