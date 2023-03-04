882d4a0a7e3747f2ffa2592db339422c0e85a126
K.Sidorov <2887458@gmail.com>
2023-03-04 21:53:08 +0300

    distance by coordinates solution

--- Practice3_methods/S_between_coordinates/Program.cs
+++ Practice3_methods/S_between_coordinates/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D плоскости
+A (3,6); B (2,1) -> 5,09
+A (7,-5); B (1,-1) -> 7,21 */
+
+// Что такое массив, и как он работает? Это упорядоченный набор элементов, который каждый у нас пронумерован.
+
+double[] A = new double[2]; // создали новый массив с двумя элементами.
+double[] B = new double[2];
+
+Console.Write($"Введите координату x точки А: ");
+double.TryParse(Console.ReadLine(), out A[0]);
+Console.Write($"Введите координату y точки А: ");
+double.TryParse(Console.ReadLine(), out A[1]);
+Console.Write($"Введите координату x точки B: ");
+double.TryParse(Console.ReadLine(), out B[0]);
+Console.Write($"Введите координату y точки B: ");
+double.TryParse(Console.ReadLine(), out B[1]);
+
+double Xdist = Math.Pow(A[0] - B[0], 2);
+double Ydist = Math.Pow(A[1] - B[1], 2);
+
+double ans = Math.Sqrt(Xdist + Ydist);
+
+System.Console.WriteLine($"A({A[0]};{A[1]}) B({B[0]};{B[1]}) -> {ans}");
