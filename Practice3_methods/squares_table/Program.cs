7239f333e1440802eb85d1feb29796e90f910267
K.Sidorov <2887458@gmail.com>
2023-03-04 23:29:37 +0300

    added solution for squares table

--- Practice3_methods/squares_table/Program.cs
+++ Practice3_methods/squares_table/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1, 4. */
+
+int N;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine(), out N);
+
+for (int i = 1; i <= N; i++)
+{
+    Console.Write($"{i * i} ");
+}
