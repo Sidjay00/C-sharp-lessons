c8a23d812ed34198e719c209e6781855835242ca
K.Sidorov <2887458@gmail.com>
2023-03-09 21:54:38 +0300

    added 2 methods fill array and print

--- Practice4_functions_and_arrays/Task_31/Program.cs
+++ Practice4_functions_and_arrays/Task_31/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* **Задача 31:** Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
+Найдите сумму отрицательных и положительных элементов массива.
+
+Например, в массиве [3,9-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20. */
+
+
+
+int[] array = FillArray(12, -9, 10);
+Print(array);
+
+
+
+
+
+
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        System.Console.Write($"{arr[i]} ");
+    System.Console.WriteLine();
+}
+
+int[] FillArray(int size, int min = 0, int max = 10)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(min, max);
+    return arr;
+}
