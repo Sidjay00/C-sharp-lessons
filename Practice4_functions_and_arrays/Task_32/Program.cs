45c25c53410ebec8344bf5b2883b9aa594b553f7
K.Sidorov <2887458@gmail.com>
2023-03-09 23:39:45 +0300

    made some work on mistakes

--- Practice4_functions_and_arrays/Task_32/Program.cs
+++ Practice4_functions_and_arrays/Task_32/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* **Задача 32:** Напишите программу замена элементов массива:
+положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+[-4.-8.8.2] -> [4.8.-8.-2] */
+
+
+
+int[] array = FillArray(12, -9, 10);
+
+Console.WriteLine($"{String.Join(", ", array)}");
+InverseElements(array);
+Console.WriteLine($"{String.Join(", ", InverseElements(array))}");
+
+
+int[] InverseElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] *= -1;
+    return arr
+}
+
+int[] FillArray(int size, int min = 0, int max = 10)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(min, max);
+    return arr;
+}
