434c1a6a45ea07d36f0bfa01ed30cb6a8897fabe
K.Sidorov <2887458@gmail.com>
2023-03-16 23:05:47 +0300

    solution for task 46

--- Practice7_recursion/Task_46/Program.cs
+++ Practice7_recursion/Task_46/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* **Задача 46:** Задайте двумерный массив размером mxn, заполненный целыми числами.
+
+ m = 3, n = 4.
+ 1 4 8 19
+ 5 -2 33 -2
+ 77 3 8 1 */
+
+ int[,] array = new int[3, 4];
+ for (int i = 0; i < array.GetLength(0); i++) //Строки
+ {
+    for (int j = 0; j < array.GetLength(1); j++) // Столбцы
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+ }
