6d9eed4fee0d8286c786a9b188acf54b00ff0fa5
K.Sidorov <2887458@gmail.com>
2023-03-16 22:01:33 +0300

    comparion of for and recursion

---
+++ Practice7_recursion/new/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Diagnostics;
+
+int n;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out n);
+
+
+Stopwatch loop = new Stopwatch();
+Stopwatch recurs = new Stopwatch();
+
+loop.Start();
+ulong left = 0;
+ulong right = 1;
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"{left} ");
+    right = left + right;
+    left = right - left;
+}
+loop.Stop();
+
+Console.WriteLine();
+
+recurs.Start();
+Console.Write(FibbRec(n));
+recurs.Stop(); 
+
+string FibbRec(int N, ulong left = 0, ulong right = 1)
+{
+    if (N <= 1)
+        return left.ToString();
+    return $"{left.ToString()} {FibbRec(N - 1, right, left + right)}";
+}
+
+
+Console.WriteLine($"\n\nloop = {loop.ElapsedMilliseconds} recurs = {recurs.ElapsedMilliseconds}");
