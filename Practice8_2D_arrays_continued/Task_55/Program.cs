7df339901242cb258831e3c0e2da59fd3275c3ca
K.Sidorov <2887458@gmail.com>
2023-03-25 23:22:08 +0300

    added multiple solution for task 55

--- Practice8_2D_arrays_continued/Task_55/Program.cs
+++ Practice8_2D_arrays_continued/Task_55/Program.cs
@@ -11,9 +11,9 @@
 7 2 2 6
 2 3 4 7 */
 
-//  Списки обращаются к динамической памяти. Есть тэг и что-то второе они хранятся в разных частях памяти.
+/*  Списки обращаются к динамической памяти. Есть тэг и что-то второе они хранятся в разных частях памяти.
 
-//  Как создается список
+Как создается список. Далее по тексту была длинная часть семинара про списки.
 List<int> arr = new List<int>();    // Так как используем динамический список, размер мы задать не может, поэтому пишем List интов. Такой строкой задаем список. Разница: прежде чем обращаться, 
 //например к пятому элементу, то выскочит ошибка. Выделяем с помощью .Add
 arr.Add(5);
@@ -35,37 +35,101 @@ else
     System.Console.WriteLine($"\nНе удалили");
     //Почему любят листы: Могут менять размер, увеличиваться или уменьшаться. И для них прописано куча методов.
 
+    // Когда лучше использовать List? С листа мы тратим больше памяти.
+    */
+
+/* Про сортировку
+
+List<int> array = new List<int>();
+
+for (int i = 0; i < 5; i++)
+    array.Add(new Random().Next(10));
+foreach (int item in array)
+    System.Console.Write($"{item} ");
+
+array.Sort((x,y) => y.CompareTo(x));    //обратная сортировка (по убыванию) через тернарный оператор
+System.Console.WriteLine();
+// array.Sort();   //  Сортировка по возрастанию
+foreach (int item in array)
+    System.Console.Write($"{item} ");
+*/
+/* Решение Ильи через листы, но оно, почему-то падает с ошибкой Unhandled exception. System.ArgumentOutOfRangeException: Index was out of range. Must be non-negative and less than the size of the collection. (Parameter 'index')
+   at System.Collections.Generic.List`1.get_Item(Int32 index)
+   at Program.<Main>$(String[] args) in /home/sysadmin/Dev/GeekBrains/C-sharp-lessons/Practice8_2D_arrays_continued/Task_55/Program.cs:line 76
 
 
-// List<List<int>> array = new List<List<int>>();
+List<List<int>> array = new List<List<int>>();
 
-// for (int i = 0; i < array.Count; i++)
+for (int i = 0; i < array.Count; i++)
+{
+    array.Add(new List<int>());
+    for (int j = 0; j < array[i].Count; j++)
+    {
+        array[i].Add(new Random().Next(10));
+        Console.Write($"{array[i][j]} ");
+    }
+    System.Console.WriteLine();
+}
+
+int LastRow = array.Count - 1;
+
+// for (int i = 0; i < array[0].Count; i++)    // Размер нужно брать внутреннего массива, поэтому мы обращаемся к нулю, а потом к каунт.
+//     (array[0][i], array[LastRow][i]) = (array[LastRow][i], array[0][i]);    // пишем не через запятую, а в разных скобках
+// так как это списки, мы можем обратиться ко всему массиву в строке, вместо того, чтобы брать каждый элемент, мы сразу можем взять всю строку
+
+(array[0], array[LastRow]) = (array[LastRow], array[0]);
+
+
+System.Console.WriteLine();
+for (int i = 0; i < array.Count; i++)
+{
+    for (int j = 0; j < array[i].Count; j++)
+        System.Console.Write($"{array[i][j]} ");
+    System.Console.WriteLine();
+} */
+
+int[,] array = new int[4, 4];
+int[,] array2 = new int[4, 4];
+
+if (array.GetLength(0) != array.GetLength(1))
+{
+    System.Console.WriteLine("Поменять строки и столбцы невозможно");
+    return;
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+//  Первое решение
+// for (int i = 0; i < array.GetLength(0); i++)
 // {
-//     array.Add(new List<int>());
-//     for (int j = 0; j < array[i].Count; j++)
+//     for (int j = i + 1; j < array.GetLength(1); j++)
 //     {
-//         array[i][j].Add(Random().Next(10));
-//         Console.Write($"{array[i][j]} ");
+//         (array[i, j], array[j, i]) = (array[j, i], array[i, j]);
 //     }
-//     System.Console.WriteLine();
 // }
 
-// int LastRow = array.Count - 1;
 
-// // for (int i = 0; i < array[0].Count; i++)    // Размер нужно брать внутреннего массива, поэтому мы обращаемся к нулю, а потом к каунт.
-// //     (array[0][i], array[LastRow][i]) = (array[LastRow][i], array[0][i]);    // пишем не через запятую, а в разных скобках
+//  Альтернативное решение
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array2[i, j] = array[j, i];
+    }
+}
 
-// // так как это списки, мы можем обратиться ко всему массиву в строке, вместо того, чтобы брать каждый элемент, мы сразу можем взять всю строку
-
-// (array[0], array[LastRow]) = (array[LastRow], array[0]);
-
-
-// System.Console.WriteLine();
-// for (int i = 0; i < array.Count; i++)
-// {
-//     for (int j = 0; j < array[i].Count; j++)
-//     {
-//         System.Console.Write($"{array[i][j]} ");
-//     }
-//     System.Console.WriteLine();
-// }
+System.Console.WriteLine();
+for (int i = 0; i < array2.GetLength(0); i++)
+{
+    for (int j = 0; j < array2.GetLength(1); j++)
+        Console.Write($"{array2[i, j]} ");
+    Console.WriteLine();
+}
