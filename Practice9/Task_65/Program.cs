4c9136ec0b85573ee90ebfa9623983f483bb332f
K.Sidorov <2887458@gmail.com>
2023-03-30 23:03:11 +0300

    added solution for Task 65

--- Practice9/Task_65/Program.cs
+++ Practice9/Task_65/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*  **Задача 65:** Задайте значение M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 5, 6, 7, 8" */
+
+using System;   // Выяснить что это значит
+
+int m, n;
+Console.Write($"Введите число M: ");
+int.TryParse(Console.ReadLine(), out m);
+Console.Write($"Введите число N: ");
+int.TryParse(Console.ReadLine(), out n);
+
+Console.WriteLine($"M = {m}; N = {n} -> {PrintNumbers(n)}");
+
+string PrintNumbers(int m, int n)
+{
+    if (m >= n)
+        return m.ToString();
+    return $"{m}, {PrintNumbers(m + 1, n)}";
+}
