ce695b68370f1ba8610400cf34868453af7c8655
K.Sidorov <2887458@gmail.com>
2023-03-30 22:44:54 +0300

    added a solution for Task 63

--- Practice9/Task_63/Program.cs
+++ Practice9/Task_63/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*  **Задача 63:** Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6" */
+
+using System;   // Выяснить что это значит
+
+int m, n;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine(), out n);
+
+Console.WriteLine($"N = {n} -> {PrintNumbers(n)}");
+
+string PrintNumbers(int n, int i = 1)
+{
+    if (i >= n)
+        return i.ToString();
+    return $"{i}, {PrintNumbers(n, i + 1)}";
+}
