6f88d45f6fa6d71f599bb46e7112d3b120584482
K.Sidorov <2887458@gmail.com>
2023-04-01 12:04:21 +0300

    started task 69

---
+++ Practice9/Task_69/Program.cs
@@ -0,0 +1,18 @@
+﻿/*  **Задача 67:** Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9 */
+
+using System;   // Выяснить что это значит
+
+int m;
+Console.Write($"Введите число M: ");
+int.TryParse(Console.ReadLine(), out m);
+
+Console.WriteLine($"{m} -> {DigitsSum(m)}");
+
+int DigitsSum(int item)
+{
+    if (item < 10)
+        return item;
+    return item % 10 + DigitsSum(item / 10);
+}
