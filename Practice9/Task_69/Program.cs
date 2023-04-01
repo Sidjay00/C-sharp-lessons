79168ca976a9260c6e2a0e76fa1b4571b712d789
K.Sidorov <2887458@gmail.com>
2023-04-01 12:21:31 +0300

    added solution for t 69

--- Practice9/Task_69/Program.cs
+++ Practice9/Task_69/Program.cs
@@ -1,18 +1,36 @@
-﻿/*  **Задача 67:** Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
-453 -> 12
-45 -> 9 */
+﻿/*  **Задача 69:** Напишите программу, которая на вход принимает два числа A и B, и возводит числа A в степень B с помощью рекурсии.
+
+А = 3; В = 5 -> 243 (3^5)
+А = 2; В = 3 -> 8 (3^5) */
 
 using System;   // Выяснить что это значит
 
-int m;
+int m, n;
 Console.Write($"Введите число M: ");
 int.TryParse(Console.ReadLine(), out m);
+Console.Write($"Введите число N: ");
+int.TryParse(Console.ReadLine(), out n);
 
-Console.WriteLine($"{m} -> {DigitsSum(m)}");
+Console.WriteLine($"{Pow(m, n)}");
 
-int DigitsSum(int item)
+double Pow(double a, int b) => b < 0 ? PowN(a, b) : PowP(a, b); //  Запись через тернарную операцию и лямбдой.
+    // if (b < 0)
+    //     return PowN(a, b);
+    // else
+    //     return PowP(a, b);
+
+double PowP(double a, int b)
 {
-    if (item < 10)
-        return item;
-    return item % 10 + DigitsSum(item / 10);
+    if (b == 0)
+        return 1;
+    if (b == 1)
+        return a;
+    return a * PowP(a, b - 1);
+}
+
+double PowN(double a, int b)
+{
+    if (b == -1)
+        return 1 / a;
+    return (double)1 / a * (double)Pow(a, b + 1);
 }
